626574196af9b2b69483024817ec4da82b72619d
Mateusz Slodki <kartax@spoko.pl>
2020-10-23 15:24:09 +0200

    zmiany

---
+++ Tekstowagra/Assets/(Scenes)/Scripts/TextGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TextGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Helo Word");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
